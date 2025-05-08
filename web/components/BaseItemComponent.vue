0173f198cd1c12038e446c735b75ed3fafaa90ac
Mikhail <meha6699@gmail.com>
2025-05-08 19:45:27 +0300

    feat(component): add BaseItemComponent and register Debt component

---
+++ web/components/BaseItemComponent.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import { resolveComponent } from "vue";
+import type { Item } from "~/utils/types";
+
+const { item, isList } = defineProps<{ item: Item; isList: boolean }>();
+
+const itemComponent = resolveComponent(item.frontmatter.type);
+</script>
+
+<template>
+  <component :is="itemComponent" :item="item" :is-list="isList" />
+</template>
