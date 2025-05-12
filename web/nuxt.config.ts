e8ad96f9f6522eb7b8fe31cdd834da5336c68f33
Mikhail <meha6699@gmail.com>
2025-05-12 15:41:31 +0300

    refactor: migrate types to pocketbase module

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
     '@nuxt/eslint',
     '@nuxt/ui',
     '@nuxtjs/mdc',
+    '~/modules/pocketbase/module',
   ],
   ssr: false,
   imports: {
--- web/utils/types.ts
+++ web/utils/types.ts
@@ -1,3 +1,5 @@
+import type { Frontmatter, ItemType } from '~/modules/pocketbase/types/types'
+
 export type Item = {
   id: string
   title: string
@@ -5,32 +7,4 @@ export type Item = {
   done: boolean
   type: ItemType
   frontmatter: Frontmatter
-}
-
-export type Frontmatter =
-  | DebtFrontmatter
-  | TrackFrontmatter
-
-export type DebtTransaction = {
-  amount: number
-  created: string
-  comment: string
-}
-
-export type DebtFrontmatter = {
-  currency: string
-  transactions: DebtTransaction[]
-}
-
-export type TrackFrontmatter = {
-  season: number
-  episode: number
-  url: string
-  next_episode: string
-}
-
-export enum ItemType {
-  Track = 'track',
-  Debt = 'debt',
-  Task = 'task',
 }
