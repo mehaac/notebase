ccd7d77343e398c87221b8b6e5fa923108f602e8
Mikhail <meha6699@gmail.com>
2025-05-28 19:29:47 +0300

    feat(components): add ItemGroceries component to the plugin

--- web/plugins/components.ts
+++ web/plugins/components.ts
@@ -1,7 +1,8 @@
 import { defineNuxtPlugin } from '#app'
-import { ItemDebt, ItemTrack } from '#components'
+import { ItemDebt, ItemTrack, ItemGroceries } from '#components'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('item-debt', ItemDebt)
   nuxtApp.vueApp.component('item-track', ItemTrack)
+  nuxtApp.vueApp.component('item-groceries', ItemGroceries)
 })
